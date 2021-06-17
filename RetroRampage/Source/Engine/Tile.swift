43b6bd728149bc823a6713c0cf11133317da13c4
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2021-06-17 18:17:14 -0400

    Add files via upload

---
+++ RetroRampage/Source/Engine/Tile.swift
@@ -0,0 +1,29 @@
+//
+//  Tile.swift
+//  Engine
+//
+//  Created by Omar Hegazy on 6/12/21.
+//  Copyright © 2021 Nick Lockwood. All rights reserved.
+//
+
+import UIKit
+
+public enum Tile: Int, Decodable
+{
+    case floor
+    case wall
+}
+
+public extension Tile
+{
+    var isWall: Bool
+    {
+        switch self
+        {
+        case .wall:
+            return true
+        case .floor:
+            return false
+        }
+    }
+}
