57e23a59d57db4dc7c7ecf0c8739640bfd647e8b
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2021-06-18 13:22:23 -0400

    Update Tilemap.swift

--- RetroRampage/Source/Engine/Tilemap.swift
+++ RetroRampage/Source/Engine/Tilemap.swift
@@ -31,4 +31,40 @@ public extension Tilemap
     {
         return tiles[y * width + x]
     }
+    
+    func hitTest(_ ray: Ray) -> Vector
+    {
+        var position = ray.origin
+        let edgeDistanceX, edgeDistanceY: Double
+        if ray.direction.x > 0
+        {
+            edgeDistanceX = position.x.rounded(.down) + 1 - position.x
+        }
+        else
+        {
+            edgeDistanceX = position.x.rounded(.up) - 1 - position.x
+        }
+        if ray.direction.y > 0
+        {
+            edgeDistanceY = position.y.rounded(.down) + 1 - position.y
+        }
+        else
+        {
+            edgeDistanceY = position.y.rounded(.up) - 1 - position.y
+        }
+        let slope = ray.direction.x / ray.direction.y
+        let step1 = Vector(x: edgeDistanceX, y: edgeDistanceX / slope)
+        let step2 = Vector(x: edgeDistanceY * slope, y: edgeDistanceY)
+        
+        if step1.length < step2.length
+        {
+            position += step1
+        }
+        else
+        {
+            position += step2
+        }
+        return position
+    }
 }
+
