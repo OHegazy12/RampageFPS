a1e5a7a6d3834c2202c3598d4fc377135af74af6
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2021-06-18 18:47:16 -0400

    Add files via upload

---
+++ RetroRampage/Source/Engine/Renderer.swift
@@ -0,0 +1,63 @@
+//
+//  Renderer.swift
+//  Engine
+//
+//  Created by Omar Hegazy on 6/12/21.
+//  Copyright © 2021 Nick Lockwood. All rights reserved.
+//
+
+public struct Renderer {
+    public private(set) var bitmap: Bitmap
+
+    public init(width: Int, height: Int) {
+        self.bitmap = Bitmap(width: width, height: height, color: .black)
+    }
+}
+
+public extension Renderer {
+    mutating func draw(_ world: World) {
+        let focalLength = 1.0
+        let viewWidth = Double(bitmap.width) / Double(bitmap.height)
+        let viewPlane = world.player.direction.orthogonal * viewWidth
+        let viewCenter = world.player.position + world.player.direction * focalLength
+        let viewStart = viewCenter - viewPlane / 2
+
+        // Cast rays
+        let columns = bitmap.width
+        let step = viewPlane / Double(columns)
+        var columnPosition = viewStart
+        for x in 0 ..< columns {
+            let rayDirection = columnPosition - world.player.position
+            let viewPlaneDistance = rayDirection.length
+            let ray = Ray(
+                origin: world.player.position,
+                direction: rayDirection / viewPlaneDistance
+            )
+            let end = world.map.hitTest(ray)
+            let wallDistance = (end - ray.origin).length
+            
+            // Draw wall
+            let wallHeight = 1.0
+            let distanceRatio = viewPlaneDistance / focalLength
+            let perpendicular = wallDistance / distanceRatio
+            let height = wallHeight * focalLength / perpendicular * Double(bitmap.height)
+            let wallColor: Color
+            if end.x.rounded(.down) == end.x
+            {
+                wallColor = .white
+            }
+            else
+            {
+                wallColor = .gray
+            }
+            bitmap.drawLine(
+                from: Vector(x: Double(x), y: (Double(bitmap.height) - height) / 2),
+                to: Vector(x: Double(x), y: (Double(bitmap.height) + height) / 2),
+                color: wallColor
+            )
+            
+            columnPosition += step
+        }
+    }
+
+}
