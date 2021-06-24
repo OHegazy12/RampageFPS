c48c178a387468f9a17ff91043cd6d19791ea09e
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2021-06-24 18:25:00 -0400

    Add files via upload

---
+++ RetroRampage/Source/Rampage/UIImage+Bitmap.swift
@@ -0,0 +1,74 @@
+//
+//  UIImage+Bitmap.swift
+//  Rampage
+//
+//  Created by Omar Hegazy on 6/12/21.
+//  Copyright © 2021 Nick Lockwood. All rights reserved.
+//
+
+import UIKit
+import Engine
+
+extension UIImage {
+    convenience init?(bitmap: Bitmap) {
+        let alphaInfo = CGImageAlphaInfo.premultipliedLast
+        let bytesPerPixel = MemoryLayout<Color>.size
+        let bytesPerRow = bitmap.width * bytesPerPixel
+
+        guard let providerRef = CGDataProvider(data: Data(
+            bytes: bitmap.pixels, count: bitmap.height * bytesPerRow
+        ) as CFData) else {
+            return nil
+        }
+
+        guard let cgImage = CGImage(
+            width: bitmap.width,
+            height: bitmap.height,
+            bitsPerComponent: 8,
+            bitsPerPixel: bytesPerPixel * 8,
+            bytesPerRow: bytesPerRow,
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGBitmapInfo(rawValue: alphaInfo.rawValue),
+            provider: providerRef,
+            decode: nil,
+            shouldInterpolate: true,
+            intent: .defaultIntent
+        ) else {
+            return nil
+        }
+
+        self.init(cgImage: cgImage)
+    }
+}
+
+extension Bitmap
+{
+    init?(image: UIImage)
+    {
+        guard let cgImage = image.cgImage
+        else
+        {
+            return nil
+        }
+        
+        let alphaInfo = CGImageAlphaInfo.premultipliedLast
+        let bytesPerPixel = MemoryLayout<Color>.size
+        let bytesPerRow = cgImage.width * bytesPerPixel
+        
+        var pixels = [Color](repeating: .clear, count: cgImage.width * cgImage.height)
+        guard let context = CGContext(
+                data: &pixels,
+                width: cgImage.width,
+                height: cgImage.height,
+                bitsPerComponent: 8,
+                bytesPerRow: bytesPerRow,
+                space: CGColorSpaceCreateDeviceRGB(),
+                bitmapInfo: alphaInfo.rawValue)
+        else
+        {
+            return nil
+        }
+        context.draw(cgImage, in: CGRect(origin: .zero, size: image.size))
+        self.init(width: cgImage.width, pixels: pixels)
+    }
+}
