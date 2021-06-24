1eed8235eb9a4bc4c38ea3defeca6df6e2bb47d4
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2021-06-24 18:24:13 -0400

    Add files via upload

---
+++ RetroRampage/Source/Engine/Animation.swift
@@ -0,0 +1,32 @@
+//
+//  Animation.swift
+//  Engine
+//
+//  Created by Omar Hegazy on 6/24/21.
+//  Copyright © 2021 Nick Lockwood. All rights reserved.
+//
+
+public struct Animation
+{
+    public let frames: [Texture]
+    public let duration: Double
+    public var time: Double = 0
+    
+    public init(frames: [Texture], duration: Double)
+    {
+        self.frames = frames
+        self.duration = duration
+    }
+}
+
+public extension Animation {
+    var texture: Texture
+    {
+        guard duration > 0 else
+        {
+            return frames[0]
+        }
+        let t = time.truncatingRemainder(dividingBy: duration) / duration
+        return frames[Int(Double(frames.count) * t)]
+    }
+}
