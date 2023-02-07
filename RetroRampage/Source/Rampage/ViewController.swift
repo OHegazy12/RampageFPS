c8125241a4022ce6b9502f9c4647e71140408ec9
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2023-02-07 00:11:13 -0500

    Update ViewController.swift

--- RetroRampage/Source/Rampage/ViewController.swift
+++ RetroRampage/Source/Rampage/ViewController.swift
@@ -14,53 +14,44 @@ private let joystickRadius: Double = 40
 private let maximumTimeStep: Double = 1 / 20
 private let worldTimeStep: Double = 1 / 120
 
-public func loadTextures() -> Textures
-{
-    return Textures(loader: { name in
-            guard let image = UIImage(named: name) else {
-                fatalError("UIImage for \(name) cannot be created")
-            }
-            guard let bitmap = Bitmap(image: image) else {
-                fatalError("Bitmap for \(name) (image: \(image)) cannot be created")
-            }
-            return bitmap
-        })
-}
-
-public func loadLevels() -> [Tilemap]
-{
+public func loadLevels() -> [Tilemap] {
     let jsonURL = Bundle.main.url(forResource: "Levels", withExtension: "json")!
     let jsonData = try! Data(contentsOf: jsonURL)
     let levels = try! JSONDecoder().decode([MapData].self, from: jsonData)
-    return levels.enumerated().map { Tilemap($0.element, index: $0.offset)}
-    //return try! JSONDecoder().decode([Tilemap].self, from: jsonData)
+    return levels.enumerated().map { Tilemap($0.element, index: $0.offset) }
 }
 
+public func loadTextures() -> Textures {
+    return Textures(loader: { name in
+        Bitmap(image: UIImage(named: name)!)!
+    })
+}
 
 class ViewController: UIViewController {
     private let imageView = UIImageView()
     private let panGesture = UIPanGestureRecognizer()
     private let tapGesture = UITapGestureRecognizer()
+    private let textures = loadTextures()
     private let levels = loadLevels()
     private lazy var world = World(map: levels[0])
     private var lastFrameTime = CACurrentMediaTime()
-    private let textures = loadTextures()
     private var lastFiredTime = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard NSClassFromString("XCTestCase") == nil
-        else
-        {
+
+        if NSClassFromString("XCTestCase") != nil {
             return
         }
+
         setUpImageView()
 
         let displayLink = CADisplayLink(target: self, selector: #selector(update))
         displayLink.add(to: .main, forMode: .common)
 
-        panGesture.delegate = self
         view.addGestureRecognizer(panGesture)
+        panGesture.delegate = self
+
         view.addGestureRecognizer(tapGesture)
         tapGesture.addTarget(self, action: #selector(fire))
         tapGesture.delegate = self
@@ -86,7 +77,11 @@ class ViewController: UIViewController {
         let timeStep = min(maximumTimeStep, displayLink.timestamp - lastFrameTime)
         let inputVector = self.inputVector
         let rotation = inputVector.x * world.player.turningSpeed * worldTimeStep
-        let input = Input(speed: -inputVector.y, rotation: Rotation(sine: sin(rotation), cosine: cos(rotation)), isFiring: lastFiredTime > lastFrameTime)
+        let input = Input(
+            speed: -inputVector.y,
+            rotation: Rotation(sine: sin(rotation), cosine: cos(rotation)),
+            isFiring: lastFiredTime > lastFrameTime
+        )
         let worldSteps = (timeStep / worldTimeStep).rounded(.up)
         for _ in 0 ..< Int(worldSteps) {
             if let action = world.update(timeStep: timeStep / worldSteps, input: input) {
@@ -105,9 +100,8 @@ class ViewController: UIViewController {
 
         imageView.image = UIImage(bitmap: renderer.bitmap)
     }
-    
-    @objc func fire(_ gestureRecognizer: UITapGestureRecognizer)
-    {
+
+    @objc func fire(_ gestureRecognizer: UITapGestureRecognizer) {
         lastFiredTime = CACurrentMediaTime()
     }
 
@@ -124,9 +118,11 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UIGestureRecognizerDelegate
-{
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+extension ViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
         return true
     }
 }
