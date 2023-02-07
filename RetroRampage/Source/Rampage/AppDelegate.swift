c1d69765bb6ed183f6d6b56d3470b07cd156f9a6
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2023-02-07 12:25:59 -0500

    Update AppDelegate.swift

--- RetroRampage/Source/Rampage/AppDelegate.swift
+++ RetroRampage/Source/Rampage/AppDelegate.swift
@@ -2,9 +2,7 @@
 //  AppDelegate.swift
 //  Rampage
 //
-//  Created by Nick Lockwood on 17/05/2019.
-//  Copyright © 2019 Nick Lockwood. All rights reserved.
-//
+// Created by Omar Hegazy
 
 import UIKit
 
